dc82db857cadea0002c5bd260db1ef817748ffc4
Wisit Srimala <palm.future@gmail.com>
2018-10-28 09:34:38 +0700

    add styles

---
+++ src/assets/styles.js
@@ -0,0 +1,78 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%'
+    },
+    bgWhite: {
+        backgroundColor: 'white'
+    },
+    fontWeightBold: {
+        fontWeight: 'bold'
+    },
+    textWhite: {
+        color: 'white'
+    },
+    textBlack: {
+        color: 'black'
+    },
+    py4: {
+        padding: '0 40'
+    },
+    py3: {
+        paddingTop: 12,
+        paddingBottom: 12,
+    },
+    opacity7: {
+        opacity: 0.7
+    },
+    display1: {
+        fontSize: 72
+    },
+    display2: {
+        fontSize: 54
+    },
+    display3: {
+        fontSize: 42
+    },
+    display4: {
+        fontSize: 28
+    },
+    display5: {
+        fontSize: 16
+    },
+    display6: {
+        fontSize: 14
+    },
+    display7: {
+        fontSize: 8
+    },
+    display8: {
+        fontSize: 4
+    },
+    fontStyleItalic: {
+        fontStyle: 'italic'
+    },
+    backDrop: {
+        height: '100%',
+        width: '100%'
+    },
+    iconLoading: {
+        height: 192,
+        width: 192
+    },
+    width80Top20: {
+        width: '80%',
+        marginTop: 20
+    },
+    rounded: {
+        borderRadius: 35
+    },
+    textCenter: {
+        textAlign: 'center',
+    }
+});
